b7fbbc3044f1c0bf5261280c55e06e1bed84268f
Lucas Buccilli <lookie27@gmail.com>
2020-10-22 22:04:25 -0400

    Adds installer

--- AudioShortcut/MainWindow.xaml.cs
+++ AudioShortcut/MainWindow.xaml.cs
@@ -1,48 +1,60 @@
-﻿using AudioSwitcher.AudioApi.CoreAudio;
+﻿using IWshRuntimeLibrary;
+using Microsoft.Toolkit.Uwp.Notifications;
 using NAudio.CoreAudioApi;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Runtime.InteropServices;
-using System.Reflection;
-using System.IO;
-using System.Resources;
-using IWshRuntimeLibrary;
-using System.Security.Principal;
+using Windows.UI.Notifications;
+using static AudioShortcut.DisplayAudioDevice;
 
 namespace AudioShortcut
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private readonly ObservableCollection<DisplayAudioDevice> audioDevices = new ObservableCollection<DisplayAudioDevice>();
+
+        private static class ArgumentCommands
+        {
+            public const string CREATE = "CREATE";
+            public const string SWITCH = "SWITCH";
+        }
         public MainWindow()
         {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 2)
+            {
+                switch (args[1])
+                {
+                    case ArgumentCommands.CREATE:
+                        {
+                            createShortcut(args[2], args[3]);
+                            break;
+                        }
+                    case ArgumentCommands.SWITCH:
+                        {
+                            switchAudioDevice(args[2]);
+                            System.Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
             InitializeComponent();
-
-            //get args here
-
             initializeActiveAudioDevices();
             audioDeviceListView.ItemsSource = audioDevices;
-
         }
 
-       private void initializeActiveAudioDevices()
+        private void initializeActiveAudioDevices()
         {
             MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
             MMDeviceCollection activeAudioDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
@@ -56,15 +68,16 @@ namespace AudioShortcut
         {
             string path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "nircmd.exe");
             System.IO.File.WriteAllBytes(path, AudioShortcut.Properties.Resources.nircmd);
+            shownNotification(deviceName);
             Process.Start(path, $"setdefaultsounddevice \"{deviceName}\" 1");
         }
 
-        private void elevatePermissions()
+        private void elevatePermissions(String deviceName, String iconName)
         {
             if (!IsElevated())
             {
                 var path = Assembly.GetExecutingAssembly().Location;
-                using (var process = Process.Start(new ProcessStartInfo(path, "/run_elevated_action")
+                using (var process = Process.Start(new ProcessStartInfo(path, $"{ArgumentCommands.CREATE} {deviceName} {iconName} /run_elevated_action")
                 {
                     Verb = "runas"
                 }))
@@ -84,14 +97,18 @@ namespace AudioShortcut
             }
         }
 
-        private void createShortcut(String deviceName)
+        private void createShortcut(String deviceName, String iconLocation)
         {
-            elevatePermissions();
+            char[] charactersToRemove = { '<', '>', ':', '"', '/', '\\', '|', '?', '*' };
+            String trimmedDeviceName = deviceName.Trim(charactersToRemove);
+            elevatePermissions(trimmedDeviceName, iconLocation);
             var startupFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             var shell = new WshShell();
-            var shortCutLinkFilePath = System.IO.Path.Combine(startupFolderPath, "CreateShortcutSample.lnk");
+            var shortCutLinkFilePath = System.IO.Path.Combine(startupFolderPath, $"{trimmedDeviceName}.lnk");
             var windowsApplicationShortcut = (IWshShortcut)shell.CreateShortcut(shortCutLinkFilePath);
+            windowsApplicationShortcut.IconLocation = $"{iconLocation}";
             windowsApplicationShortcut.Description = "AudioShortcut";
+            windowsApplicationShortcut.Arguments = $"{ArgumentCommands.SWITCH} \"{deviceName}\"";
             windowsApplicationShortcut.WorkingDirectory = System.Windows.Forms.Application.StartupPath;
             windowsApplicationShortcut.TargetPath = System.Windows.Forms.Application.ExecutablePath;
             windowsApplicationShortcut.Save();
@@ -99,7 +116,7 @@ namespace AudioShortcut
 
         private void button_MouseEnter(object sender, MouseEventArgs e)
         {
-            ((Button) sender).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2c2f33"));
+            ((Button)sender).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2c2f33"));
         }
 
         private void button_MouseLeave(object sender, MouseEventArgs e)
@@ -107,27 +124,69 @@ namespace AudioShortcut
             ((Button)sender).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
         }
 
-        private void testShortcutButton_Click(object sender, RoutedEventArgs e)
-        {
-            switchAudioDevice(((DisplayAudioDevice)audioDeviceListView.SelectedItem).FriendlyName);
-        }
-
         private void createShortcutButton_Click(object sender, RoutedEventArgs e)
         {
-            createShortcut("test");
+            if (audioDeviceListView.SelectedIndex != -1)
+            {
+                DisplayAudioDevice selectedItem = (DisplayAudioDevice)audioDeviceListView.SelectedItem;
+                createShortcut(selectedItem.FriendlyName, selectedItem.IconLocation);
+            }
+        }
+
+        private void audioDeviceListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (audioDeviceListView.SelectedIndex != -1)
+            {
+                DisplayAudioDevice selectedDevice = (DisplayAudioDevice)audioDeviceListView.SelectedItem;
+                MMType speakerType = selectedDevice.SpeakerType;
+                MMType newSpeakerType = speakerType == MMType.SPEAKER ? MMType.HEADPHONE : MMType.SPEAKER;
+                selectedDevice.SpeakerType = newSpeakerType;
+                audioDeviceListView.Items.Refresh();
+            }
+        }
+
+        private void shownNotification(String deviceName)
+        {
+            DesktopNotificationManagerCompat.RegisterAumidAndComServer<MyNotificationActivator>("LucasBuccilli.AudioShortcut");
+            DesktopNotificationManagerCompat.RegisterActivator<MyNotificationActivator>();
+
+            ToastContent toastContent = new ToastContentBuilder()
+                .AddToastActivationInfo("action=viewConversation&conversationId=5", ToastActivationType.Foreground)
+                .AddText($"Audio device set to {deviceName}")
+                .GetToastContent();
+
+            var toast = new ToastNotification(toastContent.GetXml());
+            DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
+        }
+
+        private void switchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (audioDeviceListView.SelectedIndex != -1)
+            {
+                switchAudioDevice(((DisplayAudioDevice)audioDeviceListView.SelectedItem).FriendlyName);
+            }
         }
     }
 
     public class DisplayAudioDevice
     {
+        public enum MMType
+        {
+            SPEAKER,
+            HEADPHONE
+        }
         public MMDevice device { get; }
-        public String DeviceFriendlyName { get { return this.device.DeviceFriendlyName;} }
+        public String DeviceFriendlyName { get { return this.device.DeviceFriendlyName; } }
+        public String DisplayIcon { get { return SpeakerType == MMType.SPEAKER ? "Resources/speakerIcon.png" : "Resources/headphoneIcon.png"; } }
+        public String IconLocation { get { return SpeakerType == MMType.SPEAKER ? "%systemroot%\\system32\\ddores.dll,88" : "%systemroot%\\system32\\ddores.dll,89"; } }
+        public MMType SpeakerType { get; set; }
         public String FriendlyName { get { return _FriendlyName; } }
         private String _FriendlyName;
 
         public DisplayAudioDevice(MMDevice device)
         {
             this.device = device;
+            this.SpeakerType = MMType.SPEAKER;
             this._FriendlyName = removeDeviceFriendlyName(this.device.FriendlyName);
         }
 
@@ -149,4 +208,15 @@ namespace AudioShortcut
             return value;
         }
     }
+
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+    [Guid("45824dff-aee5-4ceb-a6f3-ec52c67b64ea"), ComVisible(true)]
+    public class MyNotificationActivator : NotificationActivator
+    {
+        public override void OnActivated(string invokedArgs, NotificationUserInput userInput, string appUserModelId)
+        {
+        }
+    }
+
 }
